ff0daf3569649c5d631b07dee0715f55f9f65fae
Pepo <jmmartinezsan@gmail.com>
2021-01-04 17:28:30 +0100

    adding xsrf token

--- src/app/components/logout/logout.component.ts
+++ src/app/components/logout/logout.component.ts
@@ -9,14 +9,15 @@ import { User } from 'src/app/model/user.model';
   styleUrls: ['./logout.component.css']
 })
 export class LogoutComponent implements OnInit {
-  
+
   user = new User();
-  constructor(private router : Router) { 
+  constructor(private router : Router) {
 
   }
 
   ngOnInit(): void {
     window.sessionStorage.setItem("userdetails",null);
+    window.sessionStorage.setItem('XSRF-TOKEN', null);
     this.router.navigate(['/login']);
   }
 
